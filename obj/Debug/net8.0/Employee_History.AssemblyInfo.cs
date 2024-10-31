f9fc189945858b657e2300f59e26c1284f4b8662
Noxx2005 <167195794+Noxx2005@users.noreply.github.com>
2024-10-31 06:46:04 +0100

    database update

--- obj/Debug/net8.0/Employee_History.AssemblyInfo.cs
+++ obj/Debug/net8.0/Employee_History.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_History")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d4e69345628a0f82f1b7969a7bb8f3443aeb95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828d16166075974fe60f67d0404880c172bef5ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_History")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_History")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
